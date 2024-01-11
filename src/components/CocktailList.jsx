16e819c114920e60e52c24258760d85808b71bf7
crossroads30 <crossroads30@mail.ru>
2024-01-11 19:42:32 +0300

    feat: add map over cocktails array

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -5,6 +5,7 @@ import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
 	const { loading, cocktails } = useGlobalContext()
+	console.log(cocktails)
 	if (loading) {
 		return <Loading />
 	}
@@ -15,9 +16,14 @@ const CocktailList = () => {
 	}
 
 	return (
-		<div>
-			<h2>cocktail list component</h2>
-		</div>
+		<section className='section'>
+			<h2 className='section-title'>cocktails</h2>
+			<div className='cocktails-center'>
+				{cocktails.map(cocktail => {
+					return <Cocktail key={cocktail.id} {...cocktail} />
+				})}
+			</div>
+		</section>
 	)
 }
 
