8983a36c11dce3a598ea9308256d6eff71ff123d
crossroads30 <crossroads30@mail.ru>
2024-01-11 15:08:19 +0300

    feat: add content to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+<section className="section about-section">
+  <h1 className="section-title">about us</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure minima ad voluptatibus, neque totam laborum mollitia consectetur inventore quo libero sapiente corrupti quis facere repellat fugiat quisquam odio at ab?</p>
+</section>
   )
 }
 
