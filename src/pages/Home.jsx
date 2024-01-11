99fd62a049e51f8822be2dfe965a3e308a5a681b
crossroads30 <crossroads30@mail.ru>
2024-01-11 15:17:49 +0300

    fix: fix component import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import CocktailList from '../components/CocktailList'
 import SearchForm from '../components/SearchForm'
-import Cocktail from '../components/Cocktail'
 
 const Home = () => {
 	return (
 		<main>
 			<SearchForm />
-			<Cocktail />
+			<CocktailList />
 		</main>
 	)
 }
