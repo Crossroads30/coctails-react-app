bda8a51e97872c18c64e5946209a0878cf01dc57
crossroads30 <crossroads30@mail.ru>
2024-01-11 21:51:45 +0300

    feat: add fetching data, destruct all necessary props from data

--- src/pages/SingleCocktail.jsx
+++ src/pages/SingleCocktail.jsx
@@ -5,19 +5,63 @@ import { useParams, Link } from 'react-router-dom'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
-  const {id} = useParams()
-  const [loading, setLoading] = useState(false)
-  const [cocktail, setCocktail] = useState(null)
+	const { id } = useParams()
+	const [loading, setLoading] = useState(false)
+	const [cocktail, setCocktail] = useState(null)
 
-  useEffect(() => {
-    
-  }, [id])
+	useEffect(() => {
+		setLoading(true)
+		async function getCocktail() {
+			try {
+				const response = await fetch(`${url}${id}`)
+				const data = await response.json()
+				// console.log(data)
+				if (data.drinks) {
+					const {
+						strDrink: name,
+						strDrinkThumb: image,
+						strAlcoholic: info,
+						strCategory: category,
+						strGlass: glass,
+						strInstructions: instructions,
+						strIngredient1,
+						strIngredient2,
+						strIngredient3,
+						strIngredient4,
+						strIngredient5,
+					} = data.drinks[0]
+					const ingredients = [
+						strIngredient1,
+						strIngredient2,
+						strIngredient3,
+						strIngredient4,
+						strIngredient5,
+					]
+					const newCocktail = {
+						name,
+						image,
+						info,
+						category,
+						glass,
+						instructions,
+						ingredients,
+					}
+				} else {
+					setCocktail(null)
+				}
+			} catch (error) {
+				console.log(error)
+				setLoading(false)
+			}
+		}
+		getCocktail()
+	}, [id])
 
-  return (
-    <div>
-      <h2>{id}</h2>
-    </div>
-  )
+	return (
+		<div>
+			<h2>{id}</h2>
+		</div>
+	)
 }
 
 export default SingleCocktail
