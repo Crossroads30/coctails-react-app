9a6608f789ebce6e6339814f35cdc21f9a6fd2eb
crossroads30 <crossroads30@mail.ru>
2024-01-11 19:34:33 +0300

    feat; set fetching, set cond of displaying dat, set data props

--- src/context.jsx
+++ src/context.jsx
@@ -9,10 +9,41 @@ const AppProvider = ({ children }) => {
 	const [searchTerm, setSearchTerm] = useState('a')
 	const [cocktails, setCocktails] = useState([])
 
+	const fetchDrinks = async () => {
+		try {
+			setLoading(true)
+			const response = await fetch(`${url}${searchTerm}`)
+			const data = await response.json()
+			const { drinks } = data
+			if (drinks) {
+				const newCocktails = drinks.map(cocktail => {
+					const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+						cocktail
+					return {
+						id: idDrink,
+						name: strDrink,
+						image: strDrinkThumb,
+						info: strAlcoholic,
+						glass: strGlass,
+					}
+				})
+        setCocktails(newCocktails)
+			} else {
+				setCocktails([])
+			}
+			setLoading(false)
+		} catch (error) {
+			console.log(error)
+			setLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		fetchDrinks()
+	}, [searchTerm])
+
 	return (
-		<AppContext.Provider
-			value={{ loading, searchTerm, cocktails }}
-		>
+		<AppContext.Provider value={{ loading, searchTerm, cocktails }}>
 			{children}
 		</AppContext.Provider>
 	)
